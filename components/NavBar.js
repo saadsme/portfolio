2451b3000d7deeec0551fdcea1ed844456e6f468
xosaad <xosaad@users.noreply.github.com>
2023-01-22 16:36:22 +0400

    final chnages

--- components/NavBar.js
+++ components/NavBar.js
@@ -19,18 +19,19 @@ const NavBar = () => {
                 </button>
             </div>
         </div>
-        <div className=" flex-row justify-between items-center max-w-3xl md:my-12 md:mx-auto mb-8 w-full text-gray-700 md:flex">
-            <div className={`flex flex-col md:flex-row pt-6 text-lg space-y-2 md:space-y-0 md:space-x-8  absolute mx-3 w-[94%] md:w-full md:pl-0 pl-9 transition-all duration-500 md:static bg-gray-200 md:bg-white rounded-2xl md:z-auto z-[1000] pb-8 md:pb-0 ${open ? 'top-20' : 'top-[-490px]'}`}>
+        <div className=" flex-row justify-between  max-w-3xl md:my-12 md:mx-auto mb-8 w-full text-gray-700 md:flex">
+            <div className={`flex flex-col md:justify-start md:items-center md:flex-row pt-6 md:pt-0 text-lg space-y-2 md:space-y-0 md:space-x-8  absolute mx-3 w-[94%] md:w-full md:pl-0 pl-9 transition-all duration-500 md:static bg-gray-200 md:bg-white rounded-2xl md:z-auto z-[1000] pb-8 md:pb-0 ${open ? 'top-20' : 'top-[-490px]'}`}>
               <Link className="hover:bg-gray-200 border-spacing-1 rounded md:p-1 md:px-2 hover:text-gray-900" href={'/'}>Home</Link>
               {//<Link className="hover:bg-gray-200 border-spacing-1 rounded p-1 px-2 hover:text-gray-900" href={'/projects'}>Projects</Link>
               }
               <Link className="hover:bg-gray-200 border-spacing-1 rounded md:p-1 md:px-2 hover:text-gray-900" href={'/blog'}>Blog</Link>
               <Link className="hover:bg-gray-200 border-spacing-1 rounded md:p-1 md:px-2 hover:text-gray-900" href={'/tech'}>Tech</Link>
             </div>
+            <div>
             <button type="button" className=' md:whitespace-nowrap py-2 px-3 hidden md:flex group bg-gradient-to-r from-indigo-400 via-pink-400 to-yellow-400 hover:from-indigo-600 hover:via-pink-600 hover:to-red-600 focus:outline-none  shadow-md rounded-2xl'>
                   <Link className="underline-offset-2 decoration-dotted decoration-1 text-gray-900 group-hover:text-gray-100 text-sm underline-solid" href={'/docs/cv.pdf'} target="_blank">Resume ↗</Link>     
             </button>
-
+            </div>
         </div>
         
     
